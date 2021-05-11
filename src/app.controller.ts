c1a75ad8c3e49d5e2160492b8ff315177c216631
Nicolas Patarroyo <nicolaspatarroyo@hotmail.com>
2021-05-11 12:23:26 -0500

    Clase controladores

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,14 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('nuevo')
+  newEndpoint() {
+    return 'Yo soy nuevo';
+  }
+  
+  @Get('/ruta/')
+  hello() {
+    return 'Con /sas/';
+  }
 }
