726b021e810fe706b91cb1a8474ea461e2e16051
Nicolas Patarroyo <nicolaspatarroyo@hotmail.com>
2021-05-11 19:03:25 -0500

    Clase update, delete y formateo con ESLint

--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -1,22 +1,53 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
-    @Get('products')
-    getProducts(
-      @Query('limit') limit = 100,
-      @Query('offset') offset = 0,
-      @Query('brand') brand: string) {
-      return `Products: limit = ${limit} products = ${offset} brand = ${brand}`
-    }
+  @Get('products')
+  get(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 0,
+    @Query('brand') brand: string,
+  ) {
+    return `Products: limit = ${limit} products = ${offset} brand = ${brand}`;
+  }
 
-    @Get('filter')
-    getProductFilter() {
-        return 'Yo soy un filter';
-    }
-  
-    @Get('products/:id')
-    getProduct(@Param('id') productId: string) {
-      return `Product: ${productId}`
-    }
+  @Get('filter')
+  getFilter() {
+    return 'Yo soy un filter';
+  }
+
+  @Get('products/:id')
+  getOne(@Param('id') productId: string) {
+    return `Product: ${productId}`;
+  }
+
+  @Post()
+  create(@Body() payload: any) {
+    return {
+      message: 'Acción de crear',
+      payload,
+    };
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() payload: any) {
+    return {
+      id,
+      payload,
+    };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return id;
+  }
 }
