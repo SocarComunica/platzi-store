f36114f517b6810a1843c56f14c20799a31e320d
Nicolas Patarroyo <nicolaspatarroyo@hotmail.com>
2021-05-15 18:43:01 -0500

    Clase evitando parametros incorrectos

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // Ignores on requests noy expected parameters
+      whitelist: true,
+      // Forces error on request not expected parameters
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
