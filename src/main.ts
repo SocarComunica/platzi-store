228fe6de3e5699358a8fc506dbc1fef7b25adbe9
Nicolas Patarroyo <nicolaspatarroyo@hotmail.com>
2021-05-16 12:28:12 -0500

    Clase integrando Swagger y PartialType Open API

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       // Ignores on requests noy expected parameters
@@ -12,6 +16,16 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Platzi Store API')
+    .setDescription('Platzi Store API description')
+    .setVersion('1.0')
+    .addTag('platzi store')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
