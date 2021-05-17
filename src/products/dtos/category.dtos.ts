5f4155edae142ab5df79a9a93758879c51f69719
Nicolas Patarroyo <nicolaspatarroyo@hotmail.com>
2021-05-16 20:46:40 -0500

    Clase Relaciones Uno a Uno Embebidas

--- src/products/dtos/category.dtos.ts
+++ src/products/dtos/category.dtos.ts
@@ -1,4 +1,4 @@
-import { IsString, IsNotEmpty } from 'class-validator';
+import { IsString, IsNotEmpty, IsUrl } from 'class-validator';
 import { ApiProperty, PartialType } from '@nestjs/swagger';
 
 export class CreateCategoryDto {
@@ -6,6 +6,11 @@ export class CreateCategoryDto {
   @IsNotEmpty()
   @ApiProperty()
   readonly name: string;
+
+  @IsUrl()
+  @IsNotEmpty()
+  @ApiProperty()
+  readonly image: string;
 }
 
 export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {}
